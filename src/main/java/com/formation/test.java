162787c0b08b2410815c6a4a2c726b6b7c1f5150
Red Smi <reda.smir@outlook.com>
2021-05-12 16:33:45 +0200

    second commit

--- src/main/java/com/formation/test.java
+++ src/main/java/com/formation/test.java
@@ -4,6 +4,6 @@ public class test {
 
 	public static void main(String[] args) {
           calculator c=new calculator();
-          System.out.println(c.add(12, 75));
+          System.out.println(c.add(13, 75));
 	}
 }
